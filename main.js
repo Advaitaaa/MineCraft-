13575cb84fc4c83a6b283c41999a141a8b6e5676
Advaitaaa <73975899+Advaitaaa@users.noreply.github.com>
2021-03-20 13:10:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var canvas=new fabric.Canvas("myCanvas");
 var player_x=10;
 var player_y=10;
-var block_image_width=30;
-var block_image_height=30;
 
 var player_object="";
-var block_image_object="";
+
+body{
+	background-image: URL("https://postimg.cc/Kkn0KgMB");
+	background-position: center;
+	background-size: cover; 
+}
 
 function player_update(){
     fabric.Image.fromURL("player.png", function(Img)
@@ -25,18 +28,6 @@ function player_update(){
 
 function new_image(get_image){
     fabric.Image.fromURL(get_image, function(Img)
-    {
-        block_image_object = Img;
-	block_image_object.scaleToWidth(block_image_width);
-	block_image_object.scaleToHeight(block_image_height);
-	block_image_object.set({
-	top:player_y,
-	left:player_x
-	});
-    canvas.add(block_image_object);
-    
-	});
-
 }
 
 window.addEventListener("keydown",my_keydown);
@@ -44,22 +35,7 @@ function my_keydown(e)
 {
 	var key_pressed=e.keyCode;
 	console.log (key_pressed);
-
-	if(e.shiftkey==true && key_pressed=="80"){
-		console.log ("p and shiftkey are pressed togather");
-		block_image_width=block_image_width+10;
-		block_image_height=block_image_height+10;
-		document.getElementById("current_width").innerHTML=block_image_width;
-		document.getElementById("current_height").innerHTML=block_image_height;
-		}
-
-		if(e.shiftkey==true && key_pressed=="77"){
-			console.log ("m and shiftkey are pressed togather");
-			block_image_width=block_image_width-10;
-			block_image_height=block_image_height-10;
-			document.getElementById("current_width").innerHTML=block_image_width;
-			document.getElementById("current_height").innerHTML=block_image_height;
-			}
+}
 
 		if(key_pressed=="38")
 		{
@@ -82,33 +58,7 @@ function my_keydown(e)
 			right();
 			console.log("right");
 		}
-		if(key_pressed=="84")
-		{
-			new_image("trunk.jpg");
-			console.log("t");
-		}
-		if(key_pressed=="68")
-		{
-			new_image("dark_green.png");
-			console.log("d");
-		}
-		if(key_pressed=="76")
-		{
-			new_image("light_green.png");
-			console.log("l");
-		}
-		if(key_pressed=="71")
-		{
-			new_image("ground.png");
-			console.log("g");
-		}
-		if(key_pressed=="wall.jpng")
-		{
-			new_image("trunk.jpg");
-			console.log("t");
-		}
-
-}
+} 
 
 function up()
 {
@@ -141,18 +91,6 @@ function left()
 		player_x=player_x - block_image_width;
 		console.log("block image width = " + block_image_width);
 		console.log("When left arrow is pressed, Y = "+player_y +",X = "+player_x);
-		canvas.remove(player_object);
-		player_update();
-	}
-}
-
-function right()
-{
-	if(player_x <=900)
-	{
-		player_x=player_x + block_image_width;
-		console.log ("block image width = " + block_image_width);
-		console.log("When right arrow is pressed, Y = "+player_y +",X = "+player_x);
 		canvas.remove(player_object);
 		player_update();
 	}
